5e48ccc3b9171fd1bd0b204227f09bfe8a5792c9
Brad Cassity <bradley.cassity@gmail.com>
2020-09-26 16:34:13 -0600

    Refactored code and created additional promises

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generatePage = require('./src/page-template');
+const {writeFile, copyFile} = require('./utils/generate-site');
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -133,16 +134,39 @@ const promptUser = () => {
   };
 
   
+  // promptUser()
+  // .then(promptProject)
+  // .then(portfolioData => {
+  //   const pageHTML = generatePage(portfolioData);
+  //   fs.writeFile('./dist/index.html', pageHTML, err => {
+  //     if (err) throw err;
+  //     console.log('Portfolio complete! Check out index.html to see the output!');
+      
+  //     fs.copyFile('./src/style.css', './dist/style.css', err => {
+  //       if(err) throw err;
+  //       console.log('Moved style.css from src to dest folder.');
+  //       return;
+  //     });
+  //   });
+  // });
+
+
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw err;
-      console.log('Portfolio complete! Check out index.html to see the output!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
-
-
-
 
